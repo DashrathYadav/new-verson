f1d8580434cb9d65d5f97771cffe586e07adf644
Dashrath <sandip9028162859@gmail.com>
2023-02-18 16:15:49 +0530

    joke generator and transition

---
+++ Joke Generator/jokes.js
@@ -0,0 +1,73 @@
+
+
+// Joke Generator
+
+const url =`https://api.api-ninjas.com/v1/dadjokes?limit=${1}`;
+const api_key=`OoaMaZe7FLyEMUyiHZlhpw==svz2w8VZMdT22Tgj`
+
+const option={
+    headers: {
+        'X-Api-Key': api_key
+    }
+}
+
+async function getJokes(url) {
+    let response = await fetch(url,option);
+    let data = await response.json();
+    console.log(data);
+    return data;
+}
+ 
+
+
+ 
+
+
+
+
+
+
+// html elements linking
+
+let joke=document.getElementById("joke");
+let form= document.getElementById("btn");
+let loader=document.getElementById("loader");
+loader.style.visibility="hidden";
+
+
+form.onclick=(event)=>{
+    event.preventDefault();
+
+        loader.style.visibility="visible";
+        joke.style.visibility="hidden";
+        getJokes(url).then((res)=>{
+            console.log(res[0].joke)
+            loader.style.visibility="hidden";
+            joke.innerHTML=res[0].joke;
+            joke.style.visibility="visible";
+
+          });
+
+          
+
+}
+
+
+// on button click event
+//   form.addEventListener('submit',action);
+    
+//   function action(event)
+//   {
+//     event.preventDefault();
+    
+//         getJokes(url).then((res)=>{
+//             console.log(res)
+//           });
+
+//   }
+
+
+ 
+ 
+  console.log("I was below you still got exxecuted faster by you")
+  
