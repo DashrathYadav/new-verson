f7260be3b15c1e647877c5ae40ada2cf02e3a274
Dashrath <sandip9028162859@gmail.com>
2023-02-25 12:22:12 +0530

    protfolio1 Services section

--- rough-work/index.js
+++ rough-work/index.js
@@ -1,47 +1,78 @@
 console.log("Js file up Running")
 
+
+
+
+// ------------------------------------------------  Handling Hamburger toggle---------------------------------------------------->
 let menu=document.getElementById("menu")
 let hamburger=document.getElementById("hamburger")
 let close=document.getElementById("close")
+hamburger.onclick=()=>{
+
+    menu.classList.add("active");
+    
+    }
+    
+    close.onclick=()=>{
+        menu.classList.remove("active")
+    }
+
+
+
+
+
+
+
+// ------------------------------------------------------- Handling  Skill education tab change ----------------------------------------->
 let skill=document.getElementById("skill")
 let exp=document.getElementById("exp")
 let edu=document.getElementById("edu")
 let skills=document.getElementById("skills")
 let education=document.getElementById("education")
 let experience=document.getElementById("experience")
-
 skills.classList.remove("display")
+edu.classList.remove("info-title")
+exp.classList.remove("info-title")
 
-hamburger.onclick=()=>{
 
-menu.classList.add("active");
 
-}
-
-close.onclick=()=>{
-    menu.classList.remove("active")
-}
 
 
 skill.onclick=()=>{
     skills.classList.remove("display")
+    skill.classList.add("info-title")
     experience.classList.add("display")
+    exp.classList.remove("info-title")
     education.classList.add("display")
+    edu.classList.remove("info-title")
     console.log("1")
+    
 
 }
 
 edu.onclick=()=>{
     skills.classList.add("display")
+    skill.classList.remove("info-title")
     experience.classList.add("display")
+    exp.classList.remove("info-title")
     education.classList.remove("display")
+    edu.classList.add("info-title")
     console.log("2")
 
 }
 
 exp.onclick=()=>{
     skills.classList.add("display")
+    skill.classList.remove("info-title")
     education.classList.add("display")
+    edu.classList.remove("info-title")
     experience.classList.remove("display")
+    exp.classList.add("info-title")
     console.log("3")
 }
+
+
+
+// --------------------------------------------Handling underline of tab ------------------------------------------------------------->
+// Handling underline of tab
+
